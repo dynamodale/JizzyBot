d4118039c09b7aceebcbe54a4de555632c83e004
dynamodale <dynamodale@gmail.com>
2026-05-23 02:49:18 -0500

    Update music.js

--- src/commands/Music/music.js
+++ src/commands/Music/music.js
@@ -3,7 +3,7 @@ import { getPlayer, createPlayer, destroyPlayer } from './modules/MusicPlayer.js
 
 export default {
     data: new SlashCommandBuilder()
-        .setName('Music')
+        .setName('music')
         .setDescription('Music player controls')
 
         // /music play [query]
